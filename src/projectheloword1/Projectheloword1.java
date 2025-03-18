e0f77e37020a9b8c50b1989b2e0d9f78b93a1e75
Nasrul809 <hadinasrul809@gmail.com>
2025-03-18 19:25:51 +0700

    first commit

---
+++ src/projectheloword1/Projectheloword1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectheloword1;
+/**
+    Nama  = Achmad Nasrul Hadi
+    NIM   = 202457201003
+    Prodi = Sistem Informasi - A
+*/
+public class Projectheloword1 {
+    public static void main(String[] args) {
+        System.out.println(" Hello word ");
+        System.out.println(" Hello Achmad");
+        System.out.println(" Hello Nasrul");
+        System.out.println(" Hello Hadi ");
+    }   
+}
